d72817c09af2325bf903d21d2231837093335144
2355 <mitkit.a01@gmail.com>
2025-07-03 11:41:52 +0900

    style: fix modal

--- hackd2025-front/src/components/module/PatternDetailModal/index.tsx
+++ hackd2025-front/src/components/module/PatternDetailModal/index.tsx
@@ -101,7 +101,7 @@ export const PatternDetailModal: React.FC<PatternDetailModalProps> = ({
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50"
+      className="fixed inset-0 bg-black/50 flex items-center justify-center p-4 z-50"
       onClick={handleBackdropClick}
       role="dialog"
       aria-modal="true"
